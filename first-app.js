3b8b6761ebcaefbf413261e8bf1d75764854d830
Himanshudevg2000 <himanshudevgade87@gmail.com>
2022-08-27 00:29:17 +0530

    sending request task added

--- first-app.js
+++ first-app.js
@@ -8,9 +8,47 @@
 
 // create server
 
+// const http = require('http');
+// const server = http.createServer((req,res) => {
+//     res.write("himanshu");
+//     res.end();
+//     console.log(req.url, req.method, req.headers)
+// });
+// server.listen(4000)
+
+
+
+
+
+// sending request
+
 const http = require('http');
 const server = http.createServer((req,res) => {
-    res.write("himanshu");
+    const url = req.url;
+
+    if(url === '/home'){
+        res.write("<html>");
+        res.write("<body> <h2> Welcome home <h2> </body>");
+        res.write("</html>");
+    }
+    if(url === '/about'){
+        res.write("<html>");
+        res.write("<body> <h2> Welcome to About this page <h2> </body>");
+        res.write("</html>");
+    }
+    if(url === '/node'){
+        res.write("<html>");
+        res.write("<body> <h2> Welcome to NodeJs project <h2> </body>");
+        res.write("</html>");
+    }
+
+    res.write("<html>");
+    res.write("<title> NodeJS </title>");  // changes the title of page
+    res.write("<body> <h1> Welcome to my nodeJs project <h1> </body>");
+    res.write("</html>");
+
+    req.set
     res.end();
+    // console.log(req.url, req.method, req.headers)
 });
 server.listen(4000)
