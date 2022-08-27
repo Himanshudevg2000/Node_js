2ee539b34eec17d5f5d88829e568fc8b34b48c18
Himanshudevg2000 <himanshudevgade87@gmail.com>
2022-08-27 23:33:15 +0530

    NodeJS task 8

--- first-app.js
+++ first-app.js
@@ -26,7 +26,7 @@ const http = require('http');
 const fs = require('fs');
 const server = http.createServer((req,res) => {
     const url = req.url;
-    const method = req.mthod
+    const method = req.method
 
     if(url === '/home'){
         res.write("<html>");
@@ -48,27 +48,41 @@ const server = http.createServer((req,res) => {
         return res.end()
     }
 
-    if(url === '/text' ){
+    if(url === '/text'){
         res.write("<html>");
         res.write("<head> <title> Enter message </title> </head> ");
-        res.write("<body> <form action='/message' method='POST'> <input type='text' name='message'> <button> Create Message </button>  </form> </body>");
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>");
         res.write("</html>");
         return res.end()
     }
 
-    if(url === '/message' ){
-        fs.writeFileSync('message.txt','message created');
-        res.statusCode = 302;
-        res.setHeader('Location', '/home');
-        return res.end();
+    if(url === '/message' && method === 'POST' ){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            // console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/home');
+                return res.end();
+            });
+        });
     }
+    const readFileLines = filename =>
+    fs.readFileSync(filename)
+    .toString('UTF8')
+    const arr = readFileLines('message.txt');
+    console.log(arr);
 
     res.write("<html>");
     res.write("<title> NodeJS </title>");  // changes the title of page
     res.write("<body> <h1> Welcome to my nodeJs project <h1> </body>");
     res.write("</html>");
-
-    req.set
     res.end();
     // console.log(req.url, req.method, req.headers)
 });
